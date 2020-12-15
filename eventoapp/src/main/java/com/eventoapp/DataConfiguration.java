2a46214887834fa279bcc6df0850ff72b3c3bfb9
rookie-leo <60435208+rookie-leo@users.noreply.github.com>
2020-12-15 19:33:27 -0300

    commit DataConfiguration e EventoController OK

---
+++ eventoapp/src/main/java/com/eventoapp/DataConfiguration.java
@@ -0,0 +1,40 @@
+package com.eventoapp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		/*Configuração do BD*/
+		DriverManagerDataSource dts= new DriverManagerDataSource();
+		dts.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dts.setUrl("jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC");
+		dts.setUsername("root");
+		dts.setPassword("DevFox02");
+		
+		return dts;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		/*Configuração do Hibernate*/
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL57Dialect");
+		adapter.setPrepareConnection(true);
+		
+		return adapter;
+	}
+}
