521d865886427ce9241c388e842bbc445e7cdb9c
rookie-leo <60435208+rookie-leo@users.noreply.github.com>
2020-12-17 16:14:15 -0300

    Adição do pacote repository

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,15 +1,39 @@
 package com.eventoapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.EventoRepository;
 
 @SpringBootApplication
 @Controller
 public class EventoController {
 
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value = "/cadastrarEvento", method=RequestMethod.GET)
 	public String form(){
-		return "evento/formEvento.html";
+		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value = "/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		
+		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping(value = "/eventos", method = RequestMethod.GET)
+	public ModelAndView listaEventos() {
+		ModelAndView mav = new ModelAndView("index");//a instancia está referenciando o arquivo index.html
+		Iterable<Evento> eventos = er.findAll();
+		mav.addObject("eventos", eventos);//O primeiro arg é referente a div com o ${evento} 
+		return mav;
 	}
 }
--- eventoapp/src/main/java/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/models/Evento.java
@@ -1,7 +1,23 @@
 package com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
 	private String nome;
 	private String local;
 	private String data;
@@ -31,6 +47,13 @@ public class Evento {
 	}
 	public void setHorario(String horario) {
 		this.horario = horario;
+	}
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
 	}
 	
 }
